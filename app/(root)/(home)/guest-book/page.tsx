58d067014c893b2ce7ed4f2bfc47fd3567a7f2e5
Confidence Emonena <confidinho@yahoo.com>
2025-08-18 19:04:33 +0100

    share and calendar modal

--- app/(root)/(home)/guest-book/page.tsx
+++ app/(root)/(home)/guest-book/page.tsx
@@ -6,7 +6,7 @@ function GuestBook() {
 		<div>
 			<Header />
 
-			<div className="p-6 border rounded-lg border-[#EDE6E2] w-full sm:w-[98%] mx-auto mt-4">
+			<div className="p-4 border rounded-lg border-[#EDE6E2] w-[96%] mx-auto mt-0 sm:mt-4">
 				<BreadCrumb
 					img="/images/events.png"
 					title="GuestBook"

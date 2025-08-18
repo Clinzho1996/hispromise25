fd356fa8b9814aa6e601c61ddddc89748e20c110
Confidence Emonena <confidinho@yahoo.com>
2025-08-18 17:56:01 +0100

    share and calendar modal

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Story from "@/components/Story";
 
 function HomePage() {
 	return (
@@ -7,6 +8,7 @@ function HomePage() {
 			<Header />
 			<div className="flex flex-col  min-h-screen p-6">
 				<Hero />
+				<Story />
 			</div>
 		</div>
 	);
---
+++ components/Story.tsx
@@ -0,0 +1,79 @@
+import { Space_Grotesk } from "next/font/google";
+import Image from "next/image";
+
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
+
+function Story() {
+	return (
+		<div
+			className={`${spaceGrotesk.className} mt-4 border rounded-lg p-4 border-[#EDE6E2] flex flex-col gap-3`}>
+			<p className="text-lg sm:text-[24px] font-semibold">Our Story</p>
+
+			<div className="text-sm sm:text-base text-black flex flex-col gap-3">
+				<p className="border rounded-lg p-4 border-[#EDE6E2]">
+					<span className="text-[#DE8F8F] font-bold pr-2">
+						March 2025 - First Hello
+					</span>{" "}
+					A chance meeting blossomed into a beautiful relationship.
+				</p>
+				<p className="border rounded-lg p-4 border-[#EDE6E2]">
+					<span className="text-[#DE8F8F] font-bold pr-2">
+						April - June 2025
+					</span>{" "}
+					From road trips to late-night talks, every moment deepened our bond.
+				</p>
+				<p className="border rounded-lg p-4 border-[#EDE6E2]">
+					<span className="text-[#DE8F8F] font-bold pr-2">
+						July 2025 - The Proposal
+					</span>{" "}
+					Under the stars, one question, a ring, and a promise for eternity.
+				</p>
+			</div>
+
+			<div className="border rounded-lg p-4 border-[#EDE6E2] flex flex-col sm:flex-row justify-between items-center w-full gap-3">
+				<div className="flex flex-row justify-start items-center w-full gap-3 border rounded-lg p-3 border-[#EDE6E2]">
+					<div>
+						<Image
+							src="/images/dc.png"
+							alt="Link Preview"
+							width={100}
+							height={100}
+							className="rounded-lg"
+						/>
+					</div>
+					<div>
+						<p
+							className={`${spaceGrotesk.className} text-lg text-black font-semibold`}>
+							Dev Clinton
+						</p>
+						<p className={`${spaceGrotesk.className} text-sm text-[#A8A8A8]`}>
+							Loves music, tech guru and long walks.
+						</p>
+					</div>
+				</div>
+				<div className="flex flex-row justify-start items-center w-full gap-3 border rounded-lg p-3 border-[#EDE6E2]">
+					<div>
+						<Image
+							src="/images/pr.png"
+							alt="Link Preview"
+							width={100}
+							height={100}
+							className="rounded-lg"
+						/>
+					</div>
+					<div>
+						<p
+							className={`${spaceGrotesk.className} text-lg text-black font-semibold`}>
+							Promise
+						</p>
+						<p className={`${spaceGrotesk.className} text-sm text-[#A8A8A8]`}>
+							A heart for art and marketing guru.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Story;
